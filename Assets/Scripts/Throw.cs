569184ba78ebbe1c9e6e58abade152613da09b6d
ZhihaoJun <zhao11fs@gmail.com>
2017-05-31 11:57:18 +0800

    add polynomial collider 2d and throw

---
+++ Assets/Scripts/Throw.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Throw : MonoBehaviour {
+    public Vector2 forceDir;
+    public float power;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        if (rb) {
+            rb.AddForce(forceDir.normalized * power, ForceMode2D.Impulse);
+        }
+	}
+}
