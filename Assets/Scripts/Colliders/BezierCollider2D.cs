4f31b5a137f04d804ed4408c82d397e94bdf1f10
Ji Zihao <zhao11fs@gmail.com>
2017-05-31 19:52:21 +0800

    add basic bezier collider

---
+++ Assets/Scripts/Colliders/BezierCollider2D.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EdgeCollider2D))]
+public class BezierCollider2D : MonoBehaviour {
+	public Vector2[] points;
+	public float step = 0.05f;
+
+	private Vector2 LinearInterpolate(Vector2 a, Vector2 b, float t) {
+		return Vector2.Lerp (a, b, t);
+	}
+
+	private Vector2 Bezier(Vector2[] points, float t) {
+		int count = points.Length;
+		Vector2[] currentPoints = new Vector2[count];
+
+		// copy
+		for (int i = 0; i < points.Length; i++) {
+			currentPoints [i] = points [i];
+		}
+
+		for (int i=0; i<points.Length; i++) {
+			for (int j=0; j<=count-2; j++) {
+				Vector2 start = currentPoints [j];
+				Vector2 end = currentPoints [j+1];
+				currentPoints [j] = LinearInterpolate (start, end, t);
+			}
+			count--;
+		}
+
+		// clear references?
+
+		return currentPoints [0];
+	}
+
+	public Vector2[] EdgePoints() {
+		int count = Mathf.FloorToInt (1f / step) + 1;
+		float process = 0f;
+		Vector2[] result = new Vector2[count];
+		for (int i = 0; i < count; i++) {
+			result [i] = Bezier (points, process);
+			process += step;
+		}
+		return result;
+	}
+}
---
+++ Assets/Scripts/Editors/BezierCollider2DEditor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BezierCollider2D))]
+[CanEditMultipleObjects]
+public class BezierCollider2DEditor : Editor {
+	private SerializedProperty points;
+	private SerializedProperty step;
+	private BezierCollider2D bezierCollider;
+	private EdgeCollider2D edgeCollider;
+
+	void OnEnable() {
+		points = serializedObject.FindProperty ("points");
+		step = serializedObject.FindProperty ("step");
+		bezierCollider = (BezierCollider2D)target;
+		edgeCollider = bezierCollider.GetComponent<EdgeCollider2D> ();
+	}
+
+	public override void OnInspectorGUI() {
+		EditorGUILayout.PropertyField (points, true);
+		EditorGUILayout.PropertyField (step);
+		serializedObject.ApplyModifiedProperties ();
+		edgeCollider.points = bezierCollider.EdgePoints ();
+	}
+
+	public virtual void OnSceneGUI() {
+		EditorGUI.BeginChangeCheck ();
+		Vector3[] newPositions = new Vector3[bezierCollider.points.Length];
+		for (int i=0; i<bezierCollider.points.Length; i++) {
+			newPositions[i] = Handles.FreeMoveHandle (bezierCollider.points[i], Quaternion.identity,
+				0.1f, Vector3.one, Handles.DotHandleCap);
+		}
+		if (EditorGUI.EndChangeCheck ()) {
+			for (int i = 0; i < bezierCollider.points.Length; i++) {
+				bezierCollider.points[i] = new Vector2(newPositions[i].x, newPositions[i].y);
+			}
+			edgeCollider.points = bezierCollider.EdgePoints ();
+		}
+	}
+}
