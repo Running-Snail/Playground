2f207b2812b31b721059bb0d2a2882d2899d210b
Ji Zihao <zhao11fs@gmail.com>
2017-06-01 20:45:18 +0800

    add fps counter

---
+++ Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour {
+	public Text text;
+	public float updateRate = 1f;
+
+	private float lastTime;
+
+	void Start() {
+		lastTime = 0;
+		UpdateFPS ();
+	}
+
+	private void UpdateFPS() {
+		float fps = 1 / Time.deltaTime;
+		text.text = fps.ToString ("F1") + " FPS";
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		if (lastTime < Time.time - updateRate) {
+			UpdateFPS ();
+			lastTime = Time.time;
+		}
+	}
+}
