c99e8a47d807319f6ad5cecd9bf58794a9315db4
Ji Zihao <zhao11fs@gmail.com>
2017-06-12 23:55:11 +0800

    add Noise script

---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noise {
+	public static float Perlin(float x, float y, float z) {
+		float xy = Mathf.PerlinNoise (x, y);
+		float yz = Mathf.PerlinNoise (y, z);
+		float xz = Mathf.PerlinNoise (x, z);
+
+		// reverse
+		float yx = Mathf.PerlinNoise (y, x);
+		float zy = Mathf.PerlinNoise (z, y);
+		float zx = Mathf.PerlinNoise (z, x);
+
+		return xy + yz + xz + yx + zy + zx / 6.0f;
+	}
+}
