ca3d4c029e27580d709bfc8e222a92e679458649
ZhihaoJun <zhao11fs@gmail.com>
2017-06-14 12:54:19 +0800

    Deleta unused scroll variable

--- Assets/WaterReflection/WaterReflection.cs
+++ Assets/WaterReflection/WaterReflection.cs
@@ -8,12 +8,10 @@ public class WaterReflection : MonoBehaviour {
 	public Vector2 speed = new Vector2(0.1f, 0f);
 
 	private RenderTexture offscreenTexture;
-	private Vector2 scroll;
 
 	// Use this for initialization
 	void Start () {
 		offscreenTexture = new RenderTexture (1024, 1024, 16);
-		scroll = water.material.mainTextureOffset;
 		Vector3 size = water.bounds.size;
 		float aspect = size.x / size.y;
 		camera.aspect = aspect;
@@ -24,8 +22,6 @@ public class WaterReflection : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (water != null && camera != null) {
-			scroll += speed * Time.deltaTime;
-			// water.material.SetTextureOffset ("_NoiseTex", scroll);
 			water.material.SetTexture ("_ReflectionTex", offscreenTexture);
 		}
 	}
